9b890b7790b210a058294f30526893ff3d3a676e
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2025-06-19 18:49:00 -0700

    Initial commit

---
+++ src/main/java/pancake/explosives/ExplosivesClient.java
@@ -0,0 +1,10 @@
+package pancake.explosives;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExplosivesClient implements ClientModInitializer  {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
