205e5a0556e1b48adda8f91625f11b430dc3fcc0
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2025-06-20 17:45:20 -0700

    Added dynamite block models and textures

--- src/main/java/pancake/explosives/block/ModBlocks.java
+++ src/main/java/pancake/explosives/block/ModBlocks.java
@@ -1,19 +1,20 @@
 package pancake.explosives.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.sound.BlockSoundGroup;
 import pancake.explosives.ExplosivesEnhanced;
-import net.minecraft.sound.SoundEvents;
 
 public class ModBlocks {
 
-    public static final Block DynamiteBlock = registerBlock("dynamite_block", new Block(AbstractBlock.Settings.create().strength(1f)));
+    public static final Block DynamiteBlock = registerBlock("dynamite_block", new Block(AbstractBlock.Settings.create().sounds(BlockSoundGroup.GRASS).strength(1.0f)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -26,5 +27,9 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         ExplosivesEnhanced.LOGGER.info("Registering Mod Blocks for " + ExplosivesEnhanced.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(DynamiteBlock);
+        });
     }
 }
