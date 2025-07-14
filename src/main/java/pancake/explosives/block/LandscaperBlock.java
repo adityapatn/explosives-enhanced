8d3f178f64b2a4b721035a76f77521bdb8c72a1b
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2025-07-13 18:54:43 -0700

    dynamite block does not crash game

--- src/main/java/pancake/explosives/block/LandscaperBlock.java
+++ src/main/java/pancake/explosives/block/LandscaperBlock.java
@@ -1,6 +1,5 @@
 package pancake.explosives.block;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -22,25 +21,25 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.explosion.Explosion;
 import net.minecraft.world.explosion.ExplosionBehavior;
 import org.jetbrains.annotations.Nullable;
 import pancake.explosives.ExplosivesEnhanced;
-import net.minecraft.util.math.Vec3d;
 import pancake.explosives.registry.ModDamageTypes;
 
-public class DynamiteBlock extends Block {
+public class LandscaperBlock extends Block {
 
-    public DynamiteBlock(AbstractBlock.Settings settings) {
+    public LandscaperBlock(Settings settings) {
         super(settings);
     }
 
     protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
         if (!oldState.isOf(state.getBlock())) {
             if (world.isReceivingRedstonePower(pos)) {
-                explode(world, pos, null);
+                explode(world, pos);
                 world.removeBlock(pos, false);
             }
         }
@@ -48,48 +47,51 @@ public class DynamiteBlock extends Block {
 
     protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
         if (world.isReceivingRedstonePower(pos)) {
-            explode(world, pos, null);
+            explode(world, pos);
             world.removeBlock(pos, false);
         }
     }
 
     public void onDestroyedByExplosion(World world, BlockPos pos, Explosion explosion) {
-            explode(world, pos, null);
+        if (!world.isClient) {
+            explode(world, pos);
+        }
+    }
+
+    public void explode(World world, BlockPos pos) {
+        explode(world, pos, null);
     }
 
     public void explode(World world, BlockPos pos, @Nullable LivingEntity igniter) {
-        if (!world.isClient()) {
-            world.emitGameEvent(igniter, GameEvent.PRIME_FUSE, pos); //game event for listeners like sculk sensors
-            world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        world.emitGameEvent(igniter, GameEvent.PRIME_FUSE, pos); //game event for listeners like sculk sensors
+        world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
 
-            //The dynamite should explode
-            world.createExplosion(igniter, Explosion.createDamageSource(world, igniter), new ExplosionBehavior(), pos.getX(), pos.getY(), pos.getZ(), 4.0F, false, World.ExplosionSourceType.TNT);
-            ExplosivesEnhanced.LOGGER.info("Dynamite exploded!");
+        world.createExplosion(igniter, Explosion.createDamageSource(world, igniter), new ExplosionBehavior(), pos.getX(), pos.getY(), pos.getZ(), 4.0F, false, World.ExplosionSourceType.TNT);
+        ExplosivesEnhanced.LOGGER.info("Landscaper exploded!");
 
-            //code for registering the damage type
-            RegistryEntry<DamageType> dynamiteDamageEntry = ModDamageTypes.getDynamiteDamageType((ServerWorld) world);
-            DamageSource dynamiteDamageSource = new DamageSource(dynamiteDamageEntry, null, igniter);
+        //code for registering the damage type
+        RegistryEntry<DamageType> landscaperDamageEntry = ModDamageTypes.getLandscaperDamageType((ServerWorld) world);
+        DamageSource landscaperDamageSource = new DamageSource(landscaperDamageEntry, null, igniter);
 
-            //Since owner is excluded from explosion damage calculation, manually apply damage to owner
-            float power = 4.0F;
-            if (igniter instanceof LivingEntity livingOwner) {
-                //ExplosivesEnhanced.LOGGER.info("Damaging owner!");
-                double dx = livingOwner.getX() - pos.getX();
-                double dy = livingOwner.getEyeY() - pos.getY();
-                double dz = livingOwner.getZ() - pos.getZ();
-                double distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
+        //Since owner is excluded from explosion damage calculation, manually apply damage to owner
+        float power = 4.0F;
+        if (igniter instanceof LivingEntity livingOwner) {
+            ExplosivesEnhanced.LOGGER.info("Damaging owner!");
+            double dx = livingOwner.getX() - pos.getX();
+            double dy = livingOwner.getEyeY() - pos.getY();
+            double dz = livingOwner.getZ() - pos.getZ();
+            double distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-                float radius = power * 2.0f;
-                if (distance <= radius) {
-                    float exposure = Explosion.getExposure(Vec3d.ofCenter(pos), livingOwner);
-                    if (exposure > 0) {
-                        double normDist = distance / radius;
-                        double impact = (1.0 - normDist) * exposure;
-                        float damage = (float) ((impact * impact + impact) * 3.5 * power);
+            float radius = power * 2.0f;
+            if (distance <= radius) {
+                float exposure = Explosion.getExposure(Vec3d.ofCenter(pos), livingOwner);
+                if (exposure > 0) {
+                    double normDist = distance / radius;
+                    double impact = (1.0 - normDist) * exposure;
+                    float damage = (float)((impact * impact + impact) * 3.5 * power);
 
-                        livingOwner.damage(dynamiteDamageSource, damage); //damage source must not be explosion to affect owner
-                        //ExplosivesEnhanced.LOGGER.info("Owner damaged for " + damage + " hearts.");
-                    }
+                    livingOwner.damage(landscaperDamageSource, damage); //damage source must not be explosion to affect owner
+                    ExplosivesEnhanced.LOGGER.info("Owner damaged for " + damage + " hearts.");
                 }
             }
         }
