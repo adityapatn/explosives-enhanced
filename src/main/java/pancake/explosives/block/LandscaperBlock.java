0ac8a65d62da25fe714159e54170ba8dbdd37d72
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2025-07-13 18:57:50 -0700

    landscaper block does not crash game, landscaper does not apply damage to owner.

--- src/main/java/pancake/explosives/block/LandscaperBlock.java
+++ src/main/java/pancake/explosives/block/LandscaperBlock.java
@@ -39,7 +39,7 @@ public class LandscaperBlock extends Block {
     protected void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
         if (!oldState.isOf(state.getBlock())) {
             if (world.isReceivingRedstonePower(pos)) {
-                explode(world, pos);
+                explode(world, pos, null);
                 world.removeBlock(pos, false);
             }
         }
@@ -47,51 +47,49 @@ public class LandscaperBlock extends Block {
 
     protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
         if (world.isReceivingRedstonePower(pos)) {
-            explode(world, pos);
+            explode(world, pos, null);
             world.removeBlock(pos, false);
         }
     }
 
     public void onDestroyedByExplosion(World world, BlockPos pos, Explosion explosion) {
         if (!world.isClient) {
-            explode(world, pos);
+            explode(world, pos, null);
         }
     }
 
-    public void explode(World world, BlockPos pos) {
-        explode(world, pos, null);
-    }
-
     public void explode(World world, BlockPos pos, @Nullable LivingEntity igniter) {
-        world.emitGameEvent(igniter, GameEvent.PRIME_FUSE, pos); //game event for listeners like sculk sensors
-        world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        if (!world.isClient()) {
+            world.emitGameEvent(igniter, GameEvent.PRIME_FUSE, pos); //game event for listeners like sculk sensors
+            world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
 
-        world.createExplosion(igniter, Explosion.createDamageSource(world, igniter), new ExplosionBehavior(), pos.getX(), pos.getY(), pos.getZ(), 4.0F, false, World.ExplosionSourceType.TNT);
-        ExplosivesEnhanced.LOGGER.info("Landscaper exploded!");
+            world.createExplosion(igniter, Explosion.createDamageSource(world, igniter), new ExplosionBehavior(), pos.getX(), pos.getY(), pos.getZ(), 4.0F, false, World.ExplosionSourceType.TNT);
+            ExplosivesEnhanced.LOGGER.info("Landscaper exploded!");
 
-        //code for registering the damage type
-        RegistryEntry<DamageType> landscaperDamageEntry = ModDamageTypes.getLandscaperDamageType((ServerWorld) world);
-        DamageSource landscaperDamageSource = new DamageSource(landscaperDamageEntry, null, igniter);
+            //code for registering the damage type
+            RegistryEntry<DamageType> landscaperDamageEntry = ModDamageTypes.getLandscaperDamageType((ServerWorld) world);
+            DamageSource landscaperDamageSource = new DamageSource(landscaperDamageEntry, null, igniter);
 
-        //Since owner is excluded from explosion damage calculation, manually apply damage to owner
-        float power = 4.0F;
-        if (igniter instanceof LivingEntity livingOwner) {
-            ExplosivesEnhanced.LOGGER.info("Damaging owner!");
-            double dx = livingOwner.getX() - pos.getX();
-            double dy = livingOwner.getEyeY() - pos.getY();
-            double dz = livingOwner.getZ() - pos.getZ();
-            double distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
+            //Since owner is excluded from explosion damage calculation, manually apply damage to owner
+            float power = 4.0F;
+            if (igniter instanceof LivingEntity livingOwner) {
+                ExplosivesEnhanced.LOGGER.info("Damaging owner!");
+                double dx = livingOwner.getX() - pos.getX();
+                double dy = livingOwner.getEyeY() - pos.getY();
+                double dz = livingOwner.getZ() - pos.getZ();
+                double distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-            float radius = power * 2.0f;
-            if (distance <= radius) {
-                float exposure = Explosion.getExposure(Vec3d.ofCenter(pos), livingOwner);
-                if (exposure > 0) {
-                    double normDist = distance / radius;
-                    double impact = (1.0 - normDist) * exposure;
-                    float damage = (float)((impact * impact + impact) * 3.5 * power);
+                float radius = power * 2.0f;
+                if (distance <= radius) {
+                    float exposure = Explosion.getExposure(Vec3d.ofCenter(pos), livingOwner);
+                    if (exposure > 0) {
+                        double normDist = distance / radius;
+                        double impact = (1.0 - normDist) * exposure;
+                        float damage = (float) ((impact * impact + impact) * 3.5 * power);
 
-                    livingOwner.damage(landscaperDamageSource, damage); //damage source must not be explosion to affect owner
-                    ExplosivesEnhanced.LOGGER.info("Owner damaged for " + damage + " hearts.");
+                        livingOwner.damage(landscaperDamageSource, damage); //damage source must not be explosion to affect owner
+                        ExplosivesEnhanced.LOGGER.info("Owner damaged for " + damage + " hearts.");
+                    }
                 }
             }
         }
