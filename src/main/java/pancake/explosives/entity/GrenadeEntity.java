0bfcbe51b2de6fac9d87a5cfcdcc3b3d27444baf
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2025-07-14 21:30:51 -0700

    grenade entity death message uses correct player name and formatting (works as intended)

--- src/main/java/pancake/explosives/entity/GrenadeEntity.java
+++ src/main/java/pancake/explosives/entity/GrenadeEntity.java
@@ -4,11 +4,16 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageType;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.core.jmx.Server;
+import pancake.explosives.ExplosivesEnhanced;
 import pancake.explosives.item.ModItems;
 import net.minecraft.world.explosion.Explosion;
 import pancake.explosives.registry.ModDamageTypes;
@@ -39,7 +44,6 @@ public class GrenadeEntity extends ThrownItemEntity {
             float damagePower = 4.0F;
             Entity owner = this.getOwner();
             if (owner instanceof LivingEntity livingOwner) {
-                //ExplosivesEnhanced.LOGGER.info("Damaging owner!");
                 double dx = livingOwner.getX() - getX();
                 double dy = livingOwner.getEyeY() - getY();
                 double dz = livingOwner.getZ() - getZ();
@@ -54,10 +58,15 @@ public class GrenadeEntity extends ThrownItemEntity {
                         float damage = (float)((impact * impact + impact) * 3.5 * damagePower);
 
                         //code for creating custom damage source
-                        DamageSource grenadeDamageSource = new DamageSource(this.getWorld().getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(ModDamageTypes.GRENADE_DAMAGE_KEY), owner);
+                        //DamageSource grenadeDamageSource = new DamageSource(this.getWorld().getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(ModDamageTypes.GRENADE_DAMAGE_KEY), owner, this);
 
+                        DamageSource grenadeDamageSource = ModDamageTypes.createGrenadeEntityDamage(this.getWorld(), this, null);
+
+                        ExplosivesEnhanced.LOGGER.info("this.getOwner(): " + this.getOwner());
+                        ExplosivesEnhanced.LOGGER.info("Attacker is LivingEntity: " + (grenadeDamageSource.getAttacker() instanceof LivingEntity));
+                        ExplosivesEnhanced.LOGGER.info("DamageType: " + grenadeDamageSource.getType().msgId());
                         livingOwner.damage(grenadeDamageSource, damage); //damage source must not be explosion to affect owner
-                        //ExplosivesEnhanced.LOGGER.info("Owner damaged for " + damage + " hearts.");
+                        ExplosivesEnhanced.LOGGER.info("Owner damaged for " + damage + " hearts.");
                     }
                 }
             }
--- src/main/java/pancake/explosives/registry/ModDamageTypes.java
+++ src/main/java/pancake/explosives/registry/ModDamageTypes.java
@@ -1,25 +1,32 @@
 package pancake.explosives.registry;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 import pancake.explosives.ExplosivesEnhanced;
 
 public class ModDamageTypes {
-    public static final RegistryKey<DamageType> GRENADE_DAMAGE_KEY = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(ExplosivesEnhanced.MOD_ID, "grenade_entity"));
 
-   /*
-    public static final RegistryKey<DamageType> GRENADE_DAMAGE_KEY =
-            RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of("explosives-enhanced", "grenade_entity"));
-
-    public static RegistryEntry<DamageType> getGrenadeDamageType(ServerWorld world) {
-        RegistryWrapper<DamageType> damageTypes = world.getRegistryManager()
-                .getWrapperOrThrow(RegistryKeys.DAMAGE_TYPE);
-
-        return damageTypes.getOptional(GRENADE_DAMAGE_KEY)
-                .orElseThrow(() -> new IllegalStateException("Grenade DamageType not found"));
+    public static DamageSource createGrenadeEntityDamage(World world, Entity source, Entity attacker) {
+        return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(ExplosivesEnhanced.MOD_ID, "grenade_entity"))), source, attacker);
     }
+
+    /*
+    public static final RegistryKey<DamageType> GRENADE_DAMAGE_KEY = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(ExplosivesEnhanced.MOD_ID, "grenade_entity"));
+    public static DamageSource createGrenadeDamage(ServerWorld world, Entity attacker, Entity source) {
+        RegistryEntry<DamageType> damageTypeEntry =
+                world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(GRENADE_DAMAGE_KEY);
+        return new DamageSource(damageTypeEntry, attacker, source);
+    }
+
 
     public static final RegistryKey<DamageType> DYNAMITE_DAMAGE_KEY =
             RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of("explosives-enhanced", "dynamite_block"));
